1a959b2056e7e85d7dcf77c62ce77e903e4e9692
brittany-z <65884432+brittany-z@users.noreply.github.com>
2021-02-11 01:51:59 -0800

    Update terminal.h
    Kevin and I talked and he helped me figure these out better. So basically the terminal is just menus, and depending on the user's number and choices, then Data_center methods will be called through the data_link. We figured that the Data_center should have the provider directory since that is where the services are being added and where everything else is going on. Same reason why the pointers have also been moved to the Data_center. And then I added the functionality to the Data_center. The return types may need to be changed though. I'm not very good at coming up with meaningful ones. We thought that it wouldn't be a big deal if I just edited this branch.

--- terminal.h
+++ terminal.h
@@ -9,16 +9,33 @@ class Data_center{
 
     public:
         Data_center();
-        int validate(const std::string & input);
-        void provider_menu(const std::string & num);
-        void manager_menu(const std::string & num);
-        void interactive_menu();
+        void write_file();
+        //set is a flag to know if we need to set the pointers and permissions
+        //or not, I was thinking about maybe using this method as a search as
+        //well as a setter. Because a search method would do the same thing
+        //other than setting the pointers.
+        int validate(const std::string & input, bool set); 
+        void disp_map(int map_type);
+        void prov_report();
+        void mem_report();
+        void pull_EFT();
+        void sum_report();
+        void add();
+        void remove(); 
+        void update();
+        void add_service();
+        //void provider_menu(const std::string & num);
+        //void manager_menu(const std::string & num);
+        //void interactive_menu();
 
     private:
         char permissions;
         std::unordered_map<std::string, Provider> provider_list;
         std::unordered_map<std::string, Member> member_list;
         std::unordered_map<std::string, std::string> manager_list;
+        std::map<std::string, Service> prov_dir;
+        Provider * curr_provider;
+        Member * curr_member;
 };
 
 class Terminal{
@@ -26,14 +43,14 @@ class Terminal{
     public:
         Terminal();
         Terminal(const Data_center & link);
-        int validate(const std::string & input);
-        void provider_menu(const std::string & num);
-        void manager_menu(const std::string & num);
-        void start_menu();
+        //int validate(const std::string & input);
+        void provider_menu();
+        void manager_menu();
+        void start_menu(); //This will call validate in Data_center
     
     private:
         Data_center * data_link;
-        Provider * curr_provider;
-        Member * curr_member;
-        std::map<std::string, Service> prov_dir;
+        //Provider * curr_provider;
+        //Member * curr_member;
+        //std::map<std::string, Service> prov_dir;
 };
